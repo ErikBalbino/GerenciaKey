c1ec03b9ddb72441e1feb938cb907b1ff3fb0308
Marcio Erik Balbino da Silva <109674590+ErikBalbino@users.noreply.github.com>
2023-04-13 14:17:14 -0300

    main.py; requirements.txt
    Update previously files to latest version.

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 # imports
-# identifying user
-users = ["admin"]
-pswds = ["12345"]
-user = input("User: ")
+import sys
+import connect
+import hashpass
+
+while True:
+    inicio = input("""Welcome to GerenciaKey!
+    1 - Login
+    2 - Sign in
+    3 - Exit
+    Make your choice: """)
+    if inicio == '1':
+        break
+    elif inicio == '2':
+        print("Escolheu 2!")
+        pass
+    else:
+        print("Good bye!")
+        sys.exit()
+
 # auth user
+auth = False
+users = ["admin", "aaa"]
+pswds = ["12345", "111"]
+user = input("User: ")
 if user in users:
     main_pswd = input("Password for ({}): ".format(user))
-    if main_pswd == pswds[0]:
+    if main_pswd in pswds:
         auth = True
         print("You are now logged in!")
     else:
@@ -15,8 +34,14 @@ if user in users:
 else:
     auth = False
     print("The user does not exist!")
+
 # accessing program functions
+master = hashpass.genmaster()
+enc_teste = []
 if auth:
-    pass
+    enc_teste.append(hashpass.encrypt(pswds[0], master))
+    print(enc_teste)
+    dec_teste = hashpass.decrypt(enc_teste[0], master)
+    print(dec_teste)
 else:
     print("Program closed!")
